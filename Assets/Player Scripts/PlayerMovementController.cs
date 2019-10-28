e9b5036f28c32ef5303bee12fe56091f13dcafc2
Thestouges <thestouges@gmail.com>
2019-10-28 17:59:30 -0400

    Added player, movement, camera view, game exe

---
+++ Assets/Player Scripts/PlayerMovementController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float PlayerSpeed = 1;
+    Rigidbody Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody.velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rigidbody.velocity += -transform.right * PlayerSpeed;
+            //transform.Translate(-Vector2.right * PlayerSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rigidbody.velocity += transform.right * PlayerSpeed;
+            //transform.Translate(Vector2.right * PlayerSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Rigidbody.velocity += -transform.forward * PlayerSpeed;
+            //transform.Translate(-Vector3.forward * PlayerSpeed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Rigidbody.velocity += transform.forward * PlayerSpeed;
+            //transform.Translate(Vector3.forward * PlayerSpeed);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, 0, 200) * Time.deltaTime * PlayerSpeed/4);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 0, -200) * Time.deltaTime * PlayerSpeed/4);
+        }
+    }
+}
