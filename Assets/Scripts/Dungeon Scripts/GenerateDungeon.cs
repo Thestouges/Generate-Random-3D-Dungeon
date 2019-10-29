8c5701ca9fbe381c7b667b07afa46f271b5def1b
Thestouges <thestouges@gmail.com>
2019-10-29 02:41:16 -0400

    Now selects correct connection cube

--- Assets/Scripts/Dungeon Scripts/GenerateDungeon.cs
+++ Assets/Scripts/Dungeon Scripts/GenerateDungeon.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GenerateDungeon : MonoBehaviour
@@ -68,7 +69,7 @@ public class GenerateDungeon : MonoBehaviour
                 {
                     if (DungeonMap[i][j][k] == true)
                     {
-                        createRoom(i*2*RoomSize,j*2* RoomSize, k*2 * RoomSize);
+                        createRoom(i*2*RoomSize,j*2* RoomSize, k*2 * RoomSize, i, j, k);
                     }
                 }
             }
@@ -131,10 +132,136 @@ public class GenerateDungeon : MonoBehaviour
         }
     }
 
-    void createRoom(float posx, float posy, float posz)
+    void createRoom(float posx, float posy, float posz, int mapx, int mapy, int mapz)
     {
-        GameObject room = Instantiate(DungeonRoomsType[8]);
+        GameObject cube = roomCube(mapx, mapy, mapz);
+
+        GameObject room = Instantiate(cube);
         room.transform.position = new Vector3(posx, posy, posz);
         room.transform.localScale = new Vector3(RoomSize, RoomSize, RoomSize);
+    }
+
+    GameObject roomCube(int x, int y, int z)
+    {
+        int openings = 0;
+
+        bool[] sides = Enumerable.Repeat(false, 6).ToArray(); ;
+
+        try
+        {
+            if (DungeonMap[x + 1][y][z] == true) //front
+            {
+                sides[0] = true;
+                openings++;
+            }
+        }
+        catch
+        {
+
+        }
+
+        try
+        {
+            if (DungeonMap[x - 1][y][z] == true) //back
+            {
+                sides[1] = true;
+                openings++;
+            }
+        }
+        catch
+        {
+
+        }
+
+        try
+        {
+            if (DungeonMap[x][y + 1][z] == true) //left
+            {
+                sides[2] = true;
+                openings++;
+            }
+        }
+        catch
+        {
+
+        }
+
+        try
+        {
+            if (DungeonMap[x][y - 1][z] == true) //right
+            {
+                sides[3] = true;
+                openings++;
+            }
+        }
+        catch
+        {
+
+        }
+
+        try
+        {
+            if (DungeonMap[x][y][z + 1] == true) //up
+            {
+                sides[4] = true;
+                openings++;
+            }
+        }
+        catch
+        {
+
+        }
+
+        try
+        {
+            if (DungeonMap[x][y][z - 1] == true) //down
+            {
+                sides[5] = true;
+                openings++;
+            }
+        }
+        catch
+        {
+
+        }
+
+
+        //check
+        switch (openings)
+        {
+            case 1:
+                return DungeonRoomsType[0];
+            case 2:
+                if(sides[0] == sides[1] && sides[2] == sides[3] && sides[4] == sides[5])
+                {
+                    return DungeonRoomsType[2];
+                }
+                else
+                {
+                    return DungeonRoomsType[1];
+                }
+            case 3:
+                if (sides[0] != sides[1] && sides[2] != sides[3] && sides[4] != sides[5])
+                {
+                    return DungeonRoomsType[3];
+                }
+                else
+                {
+                    return DungeonRoomsType[4];
+                }
+            case 4:
+                if (sides[0] == sides[1] && sides[2] == sides[3] && sides[4] == sides[5])
+                {
+                    return DungeonRoomsType[5];
+                }
+                else
+                {
+                    return DungeonRoomsType[6];
+                }
+            case 5:
+                return DungeonRoomsType[7];
+            default:
+                return DungeonRoomsType[8];
+        }
     }
 }
